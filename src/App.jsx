ad8151ecda5013eecadbaedaeced74e0b69a627c
AshrayaDargarh01 <ashraya@nuchains.com>
2025-03-20 19:15:52 +0530

    Feat: Updated the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home'
+import Home from './pages/Home.jsx'
 import UserLayout from './utils/Layout/UserLayout';
 
 function App() {
